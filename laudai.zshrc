8c6958ae50eeae617bbe299b3f8648a0def27004
laudai <maryqute520@gmail.com>
2017-09-21 11:47:35 +0800

    modified workspace.sh and other script

--- laudai.zshrc
+++ laudai.zshrc
@@ -10,6 +10,9 @@ export ZSH=$HOME/.oh-my-zsh
 #ZSH_THEME="robbyrussell"
 ZSH_THEME="random"
 
+#I like the THEME:
+#cloud.zsh-theme
+
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
 
--- workspace.sh
+++ workspace.sh
@@ -14,13 +14,20 @@ tmux split-window -t workspace -h
 tmux split-window -t workspace -v -p 40
 #-p percentage
 
-#tmux select-layout -t workspace main-horizontal
+tmux new-window -t workspace -n win2 
+tmux send-keys -t workspace:2 "python" C-m
 
-tmux new-window -t workspace -n show_htop 
-tmux send-keys -t workspace:2 "htop" C-m
+tmux new-window -t workspace -n win3
+tmux send-keys -t workspace:3 "htop" C-m
 
+#tmux select-window -t workspace:1.1
 #I meet some problem,bcz I had set window-index base 1
 #but I get index 0
 #In somdeday will fix 
+
+tmux select-window -t workspace:1
+#select-window can shorten to selectw
+tmux select-pane -R
+
+#tmux select-layout -t workspace main-horizontal
 tmux attach -t workspace
-tmux select-window -t workspace:1.1 
