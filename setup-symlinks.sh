73d4408f65de7f69ad390721ed54e4afbde195f5
laudai <maryqute520@gmail.com>
2020-04-30 00:05:13 +0800

    new laudai.gitconfig , setup-symlinks.sh , setup-vscode.sh

---
+++ setup-symlinks.sh
@@ -0,0 +1,9 @@
+# Dotfiles
+ln -s ~/.dotfiles/laudai.zshrc ~/.zshrc
+ln -s ~/.dotfile/laudai.vimrc ~/.vimrc
+ln -s ~/.dotfile/laudai.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfile/laudai.gitconfig ~/.gitconfig
+
+# Visual Studio Code
+ln -s ~/.dotfile/VSCode/keybindings.json ~/.config/Code/User/keybindings.json
+ln -s ~/.dotfile/VSCode/settings.json ~/.config/Code/User/settings.json
---
+++ setup-vscode.sh
@@ -0,0 +1,6 @@
+code --install-extension ms-python.python
+code --install-extension ms-ceintl.vscode-language-pack-zh-hant
+code --install-extension visualstudioexptteam.vscodeintellicode
+code --install-extension tht13.python
+code --install-extension formulahendry.code-runner
+code --install-extension mdickin.markdown-shortcuts
