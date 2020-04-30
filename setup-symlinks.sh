b95310d2985b34864412013625a5ae49c227bffb
laudai <maryqute520@gmail.com>
2020-05-01 01:15:01 +0800

    add VSCode python snippets

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -7,6 +7,7 @@ ln -s ~/.dotfile/laudai.gitconfig ~/.gitconfig
 # Visual Studio Code
 ln -s ~/.dotfile/VSCode/keybindings.json ~/.config/Code/User/keybindings.json
 ln -s ~/.dotfile/VSCode/settings.json ~/.config/Code/User/settings.json
+ln -s ~/.dotfile/VSCode/python.json ~/.config/Code/User/snippets/python.json
 
 # initalize zsh settings
 source ~/.zshrc
