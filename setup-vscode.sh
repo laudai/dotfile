379c9a7b84c2e64cfde5849430894facaee6102f
laudai <maryqute520@gmail.com>
2020-05-25 00:22:01 +0800

    modified setup-vscode.sh

--- setup-vscode.sh
+++ setup-vscode.sh
@@ -1,3 +1,8 @@
+# Visual Studio Code
+ln -s ~/.dotfile/VSCode/keybindings.json ~/.config/Code/User/keybindings.json
+ln -s ~/.dotfile/VSCode/settings.json ~/.config/Code/User/settings.json
+ln -s ~/.dotfile/VSCode/python.json ~/.config/Code/User/snippets/python.json
+
 code --install-extension ms-python.python
 code --install-extension ms-ceintl.vscode-language-pack-zh-hant
 code --install-extension visualstudioexptteam.vscodeintellicode
