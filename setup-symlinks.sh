7261294cfe928444fd68ecc856766922529371ca
laudai <laudaihe@gmail.com>
2020-12-20 13:18:28 +0800

    link workspace.sh to /usr/local/bin

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -3,6 +3,7 @@ ln -s ~/.dotfile/laudai.zshrc ~/.zshrc
 ln -s ~/.dotfile/laudai.vimrc ~/.vimrc
 ln -s ~/.dotfile/laudai.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfile/laudai.gitconfig ~/.gitconfig
+sudo ln -s ~/.dotfile/workspace.sh /usr/local/bin/workspace
 
 # initalize zsh settings
 source ~/.zshrc
