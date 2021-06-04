675a0267359d7f45f4636563b1787a2f5f82f6f7
laudai <laudaihe@gmail.com>
2021-06-04 14:45:23 +0800

    Fixed auto install zsh plugins bash file path error.

--- install-ZSH-Plugins.sh
+++ install-ZSH-Plugins.sh
@@ -1,8 +1,9 @@
 # Install Zsh Plugins
 
 # autojump
-git clone git://github.com/wting/autojump.git && cd autojump
-./install.py
+git clone git://github.com/wting/autojump.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autojump
+cd ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autojump
+python3 ./install.py
 # zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 #  zsh-syntax-highlighting
@@ -10,6 +11,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 # zsh-navigation-tools
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/psprint/zsh-navigation-tools/master/doc/install.sh)"
 # git-open
-git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
+git clone https://github.com/paulirish/git-open.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-open
 # zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
