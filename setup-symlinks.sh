24d9568a8a07fcab1a55e7fc26f0f9eecc8e4503
laudai <laudaihe@gmail.com>
2021-06-22 22:56:43 +0800

    Set git global ignore file for vim setting. Add laudai.gitignore_global file.

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -3,6 +3,7 @@ ln -s ~/.dotfile/laudai.zshrc ~/.zshrc
 ln -s ~/.dotfile/laudai.vimrc ~/.vimrc
 ln -s ~/.dotfile/laudai.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfile/laudai.gitconfig ~/.gitconfig
+ln -s ~/.dotfile/laudai.gitignore_global ~/.gitignore_global
 sudo ln -s ~/.dotfile/workspace.sh /usr/local/bin/workspace
 
 # initalize zsh settings
