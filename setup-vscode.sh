a7221ff99ce64d5421b917f6904d757520605a9e
laudai <laudaihe@gmail.com>
2021-06-24 00:21:15 +0800

    Uninstall VSCode plugin relative-line-numbers.

--- setup-vscode.sh
+++ setup-vscode.sh
@@ -35,4 +35,3 @@ code --install-extension mikestead.dotenv  --force
 code --install-extension wayou.vscode-todo-highlight  --force
 code --install-extension editorconfig.editorconfig  --force
 code --install-extension shan.code-settings-sync  --force
-code --install-extension extr0py.vscode-relative-line-numbers --force
