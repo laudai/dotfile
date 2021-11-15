a339b22c1bb9fcf6ddb063f8d62e1fa9b968c9d3
laudai <laudaihe@gmail.com>
2021-11-16 01:03:06 +0800

    Modified install script misc.

--- install-utilities.sh
+++ install-utilities.sh
@@ -1,12 +1,13 @@
-# install oh-my-zsh framework
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # install cross-shell prompt starship
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 mkdir -p ~/.config
+echo
 echo "Download Nerd Fonts from web for starship:"
 echo "https://www.nerdfonts.com/font-downloads"
 echo
 
 # install TPM (Tmux Plugin Manager)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# install oh-my-zsh framework
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
