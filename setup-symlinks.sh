ffdd6629727d1918147bbac2b499a97021f92789
laudai <laudaihe@gmail.com>
2022-12-27 23:09:43 +0000

    [Rename] Rename the workspace.sh to linux_script/workspace.sh. Modified the setup-symlinks.sh to all file force symbol link.

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -1,11 +1,11 @@
 # Dotfiles symlinks
-ln -fs ~/.dotfile/laudai.zshrc ~/.zshrc
-ln -s ~/.dotfile/laudai.vimrc ~/.vimrc
-ln -s ~/.dotfile/laudai.tmux.conf ~/.tmux.conf
-ln -s ~/.dotfile/laudai.gitconfig ~/.gitconfig
-ln -s ~/.dotfile/laudai.gitignore_global ~/.gitignore_global
-ln -s ~/.dotfile/laudai.starship.toml ~/.config/starship.toml
-sudo ln -s ~/.dotfile/workspace.sh /usr/local/bin/workspace
+ln -sf ~/.dotfile/laudai.zshrc ~/.zshrc
+ln -sf ~/.dotfile/laudai.vimrc ~/.vimrc
+ln -sf ~/.dotfile/laudai.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfile/laudai.gitconfig ~/.gitconfig
+ln -sf ~/.dotfile/laudai.gitignore_global ~/.gitignore_global
+ln -sf ~/.dotfile/laudai.starship.toml ~/.config/starship.toml
+sudo ln -sf ~/.dotfile/linux_script/workspace.sh /usr/local/bin/workspace
 sudo ln -sf ~/.dotfile/linux_script/whatismyip.sh /usr/local/bin/whatismyip
 
 # initalize zsh settings
