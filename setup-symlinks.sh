01809d0b9ac44e569db9aca35e286ea5f693b573
laudai <laudaihe@gmail.com>
2023-03-15 21:51:04 +0000

    [Modified/New] New file laudai.ssh.config file. Modified the setup-symlinks.sh to add the personal ssh settings to the /Users/yunruhe/.ssh/config file automatically.

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -8,5 +8,12 @@ ln -sf ~/.dotfile/laudai.starship.toml ~/.config/starship.toml
 sudo ln -sf ~/.dotfile/linux_script/workspace.sh /usr/local/bin/workspace
 sudo ln -sf ~/.dotfile/linux_script/whatismyip.sh /usr/local/bin/whatismyip
 
+# setup the personal user ssh config
+if [[ $(grep "Subject: SSH personal settings" "$HOME/.ssh/config") ]]; then
+    echo "Pass for checking append the ssh config. You already added it."
+else
+    cat "$HOME/.dotfile/laudai.ssh.config" >> "$HOME/.ssh/config" &> /dev/null && echo "Append the ssh confg to $HOME/.ssh/config successfully" || echo "Something exception happend."
+fi
+
 # initalize zsh settings
 source ~/.zshrc
