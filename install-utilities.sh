637e89eacf6317ca5fc4c55e4af61b31bb40dd25
laudai <laudaihe@gmail.com>
2024-09-07 16:07:12 +0100

    [Modified] Modified the install-utilities.sh to install the brew and related package on the MacOS.

--- install-utilities.sh
+++ install-utilities.sh
@@ -11,3 +11,26 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # install oh-my-zsh framework
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install homebrew for MacOS
+[[ "$OSTYPE" == "darwin"* ]] && ! which brew && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# for mac
+if [[ "$OSTYPE" == "darwin"* ]];
+	if which brew;
+		# setup font
+		brew tap homebrew/cask-fonts && brew install --cask font-caskaydia-cove-nerd-font font-fira-code font-cascadia-code
+		brew install glances htop autojump mos gawk cloc
+		brew install --cask easy-move-plus-resize # similar or alternative to move window in the Gnome way
+		brew install --cask background-music # not work in M1 MacOS
+		brew install --cask rectangle # open source window manager in MacOS
+		brew install --cask flameshot # cross platform screeshot tools
+		brew install --cask karabiner-elements
+		# if OS is MacOS
+		ln -sf ~/.dotfile/laudai.karabiner_bash_emacs.json ~/.config/karabiner/assets/complex_modifications/bash_emacs.json
+	else
+		echo "Didn't find brew package management. Please restinall and running it again."
+	fi
+else
+	echo "Not MacOS, skip it."
+fi
