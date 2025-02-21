a4787e9697028e26a5fa80196756d9e33d6da54e
laudai <laudaihe@gmail.com>
2025-02-21 22:05:54 +0000

    [Modified] Install the VS Code extension opencclint, debug-visualizer. Remove the extension code-settings-sync.

--- setup-vscode.sh
+++ setup-vscode.sh
@@ -9,6 +9,9 @@ Now start to install the VSCode extensions.
 Will insall all VSCode extensions forcibly in the array in this script.
 "
 
+# MacOS high CPU cluprit
+# https://github.com/shanalikhan/code-settings-sync
+# https://code.visualstudio.com/docs/editor/settings-sync
 declare -a vscode_extensions=(
     "ms-python.python"
     "tht13.python"
@@ -36,7 +39,6 @@ declare -a vscode_extensions=(
     "vscode-icons-team.vscode-icons"
     "mikestead.dotenv"
     "editorconfig.editorconfig"
-    "shan.code-settings-sync"
     "hoovercj.vscode-settings-cycler"
     "gruntfuggly.todo-tree"
     "spywhere.guides"
@@ -44,6 +46,8 @@ declare -a vscode_extensions=(
     "donjayamanne.githistory"
     "leodevbro.blockman"
 	"j4ng5y.charactercount"
+	"brokenbonesdd.opencclint"
+	"hediet.debug-visualizer"
 )
 
 
