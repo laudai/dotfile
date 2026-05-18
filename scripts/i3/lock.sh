3e3fb9744b1ac02e7141f7de739051363f29dbbf
laudai <laudaihe@gmail.com>
2026-05-18 21:20:14 +0100

    chore(i3): set xset dpms 900 900 900

--- scripts/i3/lock.sh
+++ scripts/i3/lock.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# i3lock wrapper: turn off screen 60s after lock, restore 300s on unlock
+# i3lock wrapper: turn off screen 60s after lock, restore 900s on unlock
 xset dpms 60 60 60
 i3lock --nofork --image ~/Pictures/wallpaper/731739_blurred.png
-xset dpms 300 300 300
+xset dpms 900 900 900
